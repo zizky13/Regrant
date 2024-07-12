301557992dbcbbee0c61dd1a2c0df24b6205b90c
UTOPi4T <wsense911@gmail.com>
2024-07-12 23:34:27 +0700

    font changes

--- screens/tabs/Home.tsx
+++ screens/tabs/Home.tsx
@@ -10,14 +10,14 @@ const Home = () => {
     //function
 
   return (
-    <View className='flex-1 p-[10px] mt-[50px] '>
+    <View className='flex-1 p-[10px] mt-[50px] bg-whie '>
         
         <View className='flex-row items-center justify-between'>
             <TouchableOpacity onPress={() => console.log('Profile pressed!')}
                 className='flex-row items-center'>
                 <FontAwesome6 name="user-circle" size={40} color="#252525" marginHorizontal={5}  />
-                    <View className='mx-[10px]'>
-                        <Text className='text-[16px] font-medium text-mainText'> Reza</Text>
+                    <View className='mx-[8px]'>
+                        <Text className='text-[16px] font-pmedium text-mainText'> Reza</Text>
                         <View className='flex-row items-center'>
                             <Entypo name="star" size={15} color="#299B65" />
                             <Text className='text-subhead'> 4.5</Text>
@@ -41,13 +41,14 @@ const Home = () => {
         
         <View className='mt-[20px] mb-[22px]' >
             <Text style={{
-                fontSize: 38,
-                fontWeight: 'bold',
-                color: '#252525',
-                marginLeft:2,
-                
-            }}>Offer / lend / borrow.</Text>
-            <Text className='text-[17px] text-subhead p-[6px] leading-[20px]' >
+                // fontSize: 38,
+                // fontWeight: 'bold',
+                // color: '#252525',
+                // marginLeft:2,
+                }}
+                className='text-[32px] font-psemibold ml-[2px]'
+                >Offer / lend / borrow.</Text>
+            <Text className='text-subhead text-[15px] p-[6px] leading-[20px] font-pregular' >
                 Discover what you desire in a whole new way! Barter your pre-loved items for something fresh, or buy and sell with ease and security
 
             </Text>
@@ -73,7 +74,7 @@ const TopTabs = ({ setActiveTab, activeTab }) => {
                 className=' border-b-mainText ml-[10px] mr-[25px] pb-[3px]'
                 style={{ borderBottomWidth: activeTab === 'offer' ? 2 : 0 }}
             >
-                <Text className='text-[25px] font-medium'
+                <Text className='text-[25px] font-pmedium'
                     style={{ color: activeTab === 'offer' ? '#252525' : '#707070' }}
                 > Offer </Text>
             </TouchableOpacity>
@@ -81,7 +82,7 @@ const TopTabs = ({ setActiveTab, activeTab }) => {
                 style={{ borderBottomWidth: activeTab === 'request' ? 2 : 0, }}
                 className=' border-b-mainText pb-[3px]'   
             >
-                <Text className='text-[25px] font-medium'
+                <Text className='text-[25px] font-pmedium'
                     style={{ color: activeTab === 'request' ? '#252525' : '#707070'}}
                     
                 > Request</Text>
@@ -98,31 +99,34 @@ const OfferCard = () => {
           id: 1,
           title: "Deez Watch",
           image: require('../../assets/images/logo.png'),
-          condition: "Used",
-          conditionPercentage: 98,
+          status: "Used",
+          statusPercentage: 98,
           get: true,
           borrow: true,
-          distance: "500m"
+          distance: "500m",
+          chatIcon: icons.chat,
         },
         {
           id: 2,
           title: "Dummy 2nd",
           image: require('../../assets/images/logo.png'),
-          condition: "Like new",
-          conditionPercentage: 100,
+          status: "Like new",
+          statusPercentage: 100,
           get: false,
           borrow: true,
-          distance: "1km"
+          distance: "1.5km",
+          chatIcon: icons.chat,
         },
         {
           id: 3,
           title: "Dummy 3rd",
           image: require('../../assets/images/logo.png'),
-          condition: "Refurbished",
-          conditionPercentage: 90,
+          status: "Refurbished",
+          statusPercentage: 90,
           get: true,
           borrow: false,
-          distance: "2km"
+          distance: "2km",
+          chatIcon: icons.chat,
         }
       ];
     return (
@@ -137,33 +141,34 @@ const OfferCard = () => {
                     className='w-[125px] h-[135px] rounded-[5px] '
                     style={{ resizeMode: 'contain' }}
                 />
-                <View className='ml-[10px] my-[2px] justify-between'>
-                    <View>
-                        <Text className='text-[22px] font-medium text-mainText'>{item.title}</Text>
-                        <View className='flex-row items-center my-[2px]'>
-                            <View className='flex-row items-center mr-[20px]'>
-                                <Ionicons name="pulse" size={20} color="#808080" />
-                                <Text className='text-subhead text-[12px] font-medium ml-[5px]'>{item.condition}</Text>
-                            </View>
+                <View className='ml-[10px] my-[6px] justify-between'>
+                    <View className='flex-row items-start justify-between'>
+                        <View>
+                            <Text className='text-[20px] font-pmedium text-mainText '>{item.title}</Text>
                             <View className='flex-row items-center'>
-                                <FontAwesome5 name="heartbeat" size={15} color="#808080" />
-                                <Text className='text-subhead text-[12px] font-medium ml-[5px]'> Condition {item.conditionPercentage}%</Text>
+                                <Ionicons name="pulse" size={22} color="#808080" />
+                                <Text className='text-subhead text-[13px] font-pregular ml-[5px]'>{item.status}</Text>
+                                
                             </View>
                         </View>
+                        <Image source={item.chatIcon}
+                            className='w-[20px] h-[20px] mr-[22px] mt-[5px]'
+                        />
+                        
                     </View>
                     <View className='my-[5px]'>
                         <View className='flex-row items-end'>
                             <View className='flex-row items-center mr-[15px]'>
                                 <FontAwesome5 name="shopping-cart" size={14} color="#A7A7A7" />
-                                <Text className='text-disabled text-[12px] font-normal ml-[5px]'> Get</Text>
+                                <Text className='text-disabled text-[13px] font-pregular ml-[5px]'> Get</Text>
                             </View>
                             <View className='flex-row items-end'>
                                 <FontAwesome5 name="hand-holding" size={18} color="#299B65" marginBottom={5} />
-                                <Text className='text-primary text-[12px] font-normal ml-[5px]'> Borrow</Text>
+                                <Text className='text-primary text-[13px] font-pregular ml-[5px]'> Borrow</Text>
                             </View>
-                            <View className='flex-row ml-[35px]'>
+                            <View className='flex-row mx-[20px]'>
                                 <Ionicons name="location-sharp" size={18} color="#252525" />
-                                <Text className='text-mainText text-[13px]'>{item.distance}</Text>
+                                <Text className='text-mainText text-[13px] font-pregular'>{item.distance}</Text>
                             </View>
                         </View>
                     </View>
@@ -219,14 +224,10 @@ const RequestCard = () => {
                         className='w-[78px] h-[78px] rounded-[5px]'
                         style={{ resizeMode: 'contain' }}
                     />
-                    <View className='flex-1 ml-[10px] my-[15px]'>
-                        <Text className='text-[20px] font-medium text-mainText'>
-                        {item.title}
-                        </Text>
+                    <View className='flex-1 ml-[12px] my-[15px]'>
+                        <Text className='text-[20px] font-pmedium text-mainText'>{item.title}</Text>
                         <View className='border-b-primary border-b-[1px] self-start'>
-                        <Text className='text-subhead font-normal ml-[5px] my-[2px]'>
-                            {item.description}
-                        </Text>
+                            <Text className='text-subhead text-[13px] font-pregular ml-[2px] '>{item.description}</Text>
                         </View>
                     </View>
                     <View className='mx-[10px]'>
