2356e14c91ea6fd1d8d92ddeec3d76cb89797294
UTOPi4T <wsense911@gmail.com>
2024-07-12 11:32:33 +0700

    Home page interface

--- screens/tabs/Home.tsx
+++ screens/tabs/Home.tsx
@@ -2,11 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+import Beranda from '../pages/home/beranda'
+
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>home</Text>
-    </SafeAreaView>
+   
+    <Beranda/>
+    
   )
 }
 
